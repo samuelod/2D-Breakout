65870bf607a871b3d32d54ba4eb751be7f098b2f
Sam Odulaja <samuelod@buffalo.edu>
2023-03-25 16:33:58 -0400

    drew paddle

--- script.js
+++ script.js
@@ -4,5 +4,26 @@ const ctx = canvas.getContext("2d");
 cvs.height = 500;
 cvs.width = 400;
 
+//GAME VARIABLES/CONSTANTS
+const paddleWidth = 100;
+const paddleBottom = 50;
+const paddleHeight = 20;
 
+// CREATE THE PADDLE 
+const paddle = {
+  x: cvs.width/2 - paddleWidth/2,
+  y: cvs.height - paddleBottom - paddleHeight, //paddleBottom is distance btwn the bottom margin * bottom of paddle 
+  width: paddleWidth,
+  height: paddleHeight,
+  dx: 5
+}
+
+function drawPaddle(){
+  ctx.fillStyle = "#2e3548";
+  ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+  ctx.strokeStyle = "#ff494e";
+  ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
+
+drawPaddle();
 
